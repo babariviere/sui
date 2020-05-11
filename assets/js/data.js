86467b1bea6c00894766399502cc907ec7c93849
Bastien Rivière <babathriviere@gmail.com>
2020-05-11 23:44:15 +0200

    Formatting + add configurable providers
    Providers from providers.json now can be used for search.

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,35 +1,33 @@
 var data_links = "links.json";
-  
-$(document).ready(function(){
-  $.getJSON(data_links,
-    function (data) {
-      var mysource = $('#links-template').html();
-      var mytemplate = Handlebars.compile(mysource);
-      var myresult = mytemplate(data)
-      $('#links').html(myresult);
+
+$(document).ready(function() {
+  $.getJSON(data_links, function(data) {
+    var mysource = $("#links-template").html();
+    var mytemplate = Handlebars.compile(mysource);
+    var myresult = mytemplate(data);
+    $("#links").html(myresult);
   });
 });
 
 var data_apps = "apps.json";
-  
-$(document).ready(function(){
-  $.getJSON(data_apps,
-    function (data) {
-      var mysource = $('#apps-template').html();
-      var mytemplate = Handlebars.compile(mysource);
-      var myresult = mytemplate(data)
-      $('#apps').html(myresult);
+
+$(document).ready(function() {
+  $.getJSON(data_apps, function(data) {
+    var mysource = $("#apps-template").html();
+    var mytemplate = Handlebars.compile(mysource);
+    var myresult = mytemplate(data);
+    $("#apps").html(myresult);
   });
 });
 
 var data_providers = "providers.json";
-  
-$(document).ready(function(){
-  $.getJSON(data_providers,
-    function (data) {
-      var mysource = $('#providers-template').html();
-      var mytemplate = Handlebars.compile(mysource);
-      var myresult = mytemplate(data)
-      $('#providers').html(myresult);
+
+$(document).ready(function() {
+  $.getJSON(data_providers, function(data) {
+    window.providers = data["providers"];
+    var mysource = $("#providers-template").html();
+    var mytemplate = Handlebars.compile(mysource);
+    var myresult = mytemplate(data);
+    $("#providers").html(myresult);
   });
 });
